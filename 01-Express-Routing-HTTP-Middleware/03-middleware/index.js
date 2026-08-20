992148e8fca8f63c625b0a832607d91f0e3ca3d1
Anas Shaik <anasshaik382@gmail.com>
2026-08-20 21:18:59 +0530

    Update index.js

--- 01-Express-Routing-HTTP-Middleware/03-middleware/index.js
+++ 01-Express-Routing-HTTP-Middleware/03-middleware/index.js
@@ -1,1 +1,24 @@
+const express = require("express");
 
+const app = express();
+
+// Custom Middleware
+function loggerMiddleware(req, res, next) {
+    console.log("First middleware is executed in console");
+
+    // Pass control to the next middleware or route
+    next();
+}
+
+// Register Middleware
+app.use(loggerMiddleware);
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Hello from Express with middleware!");
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});

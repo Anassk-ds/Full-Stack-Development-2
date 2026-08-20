7e2c6cf1d20ef76cff4119f08443a4435c8bfb1c
Anas Shaik <anasshaik382@gmail.com>
2026-08-20 21:15:00 +0530

    Implement basic CRUD operations with Express

--- 01-Express-Routing-HTTP-Middleware/02-http-methods/index.js
+++ 01-Express-Routing-HTTP-Middleware/02-http-methods/index.js
@@ -1,1 +1,38 @@
+const express = require("express");
 
+const app = express();
+
+let items = [];
+
+// Add Item
+app.get("/add/:name", (req, res) => {
+    const newItem = {
+        id: Date.now(),
+        name: req.params.name
+    };
+
+    items.push(newItem);
+
+    res.send(
+        `Added item: ${newItem.name} with id ${newItem.id}`
+    );
+});
+
+// Retrieve All Items
+app.get("/items", (req, res) => {
+    res.json(items);
+});
+
+// Delete Item
+app.get("/delete/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    items = items.filter(item => item.id !== id);
+
+    res.send(`Deleted item with id ${id}`);
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
+});

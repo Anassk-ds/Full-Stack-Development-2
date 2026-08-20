57406322237b9822b99abbbad75840e5934d37c0
Anas Shaik <anasshaik382@gmail.com>
2026-08-20 21:09:47 +0530

    Update index.js

--- 01-Express-Routing-HTTP-Middleware/01-routing/index.js
+++ 01-Express-Routing-HTTP-Middleware/01-routing/index.js
@@ -1,1 +1,36 @@
+const express = require("express");
 
+const app = express();
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Hey still alive ?");
+});
+
+// About Route
+app.get("/about", (req, res) => {
+    res.send("about page");
+});
+
+// Route Parameter
+app.get("/user/:name", (req, res) => {
+    const usersname = `${req.params.name}`;
+    res.send(`username is : ${usersname}`);
+});
+
+// URL Building
+app.get("/using/:name", (req, res) => {
+    const usersname = `/user/${req.params.name}?view=full`;
+    res.send(`username is : ${usersname}`);
+});
+
+// Query Parameter
+app.get("/search", (req, res) => {
+    const key = req.query.keyword;
+    res.send(`You searched for "${key}"`);
+});
+
+// Start Server
+app.listen(3000, () => {
+    console.log("Running on http://localhost:3000");
+});

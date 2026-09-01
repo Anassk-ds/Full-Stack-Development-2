89d6d85ebedc590eb73194a410333186b457f490
Anas Shaik <anasshaik382@gmail.com>
2026-09-01 22:15:14 +0530

    Update App.jsx

--- 05-React-Basics/02-jsx-expressions/src/App.jsx
+++ 05-React-Basics/02-jsx-expressions/src/App.jsx
@@ -1,1 +1,16 @@
+import React from "react";
 
+function App() {
+  const name = "Bahubali";
+  const age = 50;
+
+  return (
+    <div>
+      <h1>
+        Welcome {name} & your age is : {age}
+      </h1>
+    </div>
+  );
+}
+
+export default App;

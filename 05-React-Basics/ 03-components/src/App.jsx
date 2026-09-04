00828a6a8c4e3b00e9d56cf162c31d49e88df415
Anas Shaik <anasshaik382@gmail.com>
2026-09-04 23:14:46 +0530

    Add About and Home components to App

--- 05-React-Basics/ 03-components/src/App.jsx
+++ 05-React-Basics/ 03-components/src/App.jsx
@@ -1,1 +1,20 @@
+import React from "react";
 
+import About from "./About";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+
+      <h1>App Page</h1>
+
+      <About />
+
+      <Home />
+
+    </div>
+  );
+}
+
+export default App;

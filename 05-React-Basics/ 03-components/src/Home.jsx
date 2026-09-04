561f7eef100a41374364836e85c6e392d191d85e
Anas Shaik <anasshaik382@gmail.com>
2026-09-04 23:14:11 +0530

    Update Home.jsx

--- 05-React-Basics/ 03-components/src/Home.jsx
+++ 05-React-Basics/ 03-components/src/Home.jsx
@@ -1,1 +1,13 @@
+import React from "react";
 
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home Page Class Component!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;

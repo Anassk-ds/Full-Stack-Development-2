45909655d6afa98863ba9eb32e85832a02605204
Anas Shaik <anasshaik382@gmail.com>
2026-09-06 21:43:19 +0530

    Add state management and update function in App component

--- 06-React-Props-State-Styles-Events/src/App.jsx
+++ 06-React-Props-State-Styles-Events/src/App.jsx
@@ -1,1 +1,35 @@
+import React, { useState } from "react";
+import About from "./About";
 
+function App() {
+  const [data, setData] = useState("Bahubali");
+
+  function updateData() {
+    setData("User");
+  }
+
+  return (
+    <div>
+
+      <h1>Props and State</h1>
+
+      <h2>Props Example</h2>
+
+      <About name="Bahubali" age="56" />
+      <About name="Katappa" age="85" />
+      <About name="Prabhas" />
+      <About name="User" />
+
+      <h2>State Example</h2>
+
+      <h3>{data}</h3>
+
+      <button onClick={updateData}>
+        Change Data
+      </button>
+
+    </div>
+  );
+}
+
+export default App;

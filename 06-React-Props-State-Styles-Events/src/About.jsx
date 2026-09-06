c30e632483595fe5738868bfaf5f832ca49859a6
Anas Shaik <anasshaik382@gmail.com>
2026-09-06 21:42:49 +0530

    Update About.jsx

--- 06-React-Props-State-Styles-Events/src/About.jsx
+++ 06-React-Props-State-Styles-Events/src/About.jsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+function About(props) {
+  return (
+    <div>
+      <h3>
+        About Component Created By: {props.name}
+        {props.age && ` & age: ${props.age}`}
+      </h3>
+    </div>
+  );
+}
+
+export default About;

90f2c97560dfa9326985baa367594a68d0a9de7b
Anas Shaik <anasshaik382@gmail.com>
2026-09-15 10:54:01 +0530

    Add inline, internal, and external CSS styles

--- 06-React-Props-State-Styles-Events/02-react-styling/src/App.jsx
+++ 06-React-Props-State-Styles-Events/02-react-styling/src/App.jsx
@@ -1,1 +1,40 @@
+import React from "react";
 
+import "./index.css";
+
+function App() {
+
+  const internalCss = {
+    color: "green",
+    backgroundColor: "yellow",
+    margin: "20px",
+    border: "2px solid red",
+    padding: "10px"
+  };
+
+  return (
+    <div>
+
+      <h1
+        style={{
+          color: "red",
+          backgroundColor: "pink",
+          padding: "10px"
+        }}
+      >
+        CSS Inline Way
+      </h1>
+
+      <h2 style={internalCss}>
+        CSS Internal Way
+      </h2>
+
+      <h3 className="external-style">
+        External Way of CSS - Some Content
+      </h3>
+
+    </div>
+  );
+}
+
+export default App;

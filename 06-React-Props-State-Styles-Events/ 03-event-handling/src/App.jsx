9f14e022a65d829a9a60c9f80abb5819e53b5fe5
Anas Shaik <anasshaik382@gmail.com>
2026-09-16 09:29:00 +0530

    Update App.jsx

--- 06-React-Props-State-Styles-Events/ 03-event-handling/src/App.jsx
+++ 06-React-Props-State-Styles-Events/ 03-event-handling/src/App.jsx
@@ -1,1 +1,40 @@
+import React, { useState } from "react";
 
+function App() {
+
+  const [data, setData] = useState(0);
+
+  function onclickUpdate() {
+    setData(data + 1);
+  }
+
+  function handleMouseEnter() {
+    setData(data + 1);
+  }
+
+  function reset() {
+    setData(0);
+  }
+
+  return (
+    <div>
+
+      <h1>{data}</h1>
+
+      <button onClick={onclickUpdate}>
+        onClick Update
+      </button>
+
+      <button onMouseEnter={handleMouseEnter}>
+        Mouse Enter
+      </button>
+
+      <button onClick={reset}>
+        Reset
+      </button>
+
+    </div>
+  );
+}
+
+export default App;

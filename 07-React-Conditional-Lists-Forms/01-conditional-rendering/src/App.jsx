b04987d13010bc5402c76f95ba029506ad42658d
Anas Shaik <anasshaik382@gmail.com>
2026-09-17 09:40:41 +0530

    Implement conditional rendering for login state

--- 07-React-Conditional-Lists-Forms/01-conditional-rendering/src/App.jsx
+++ 07-React-Conditional-Lists-Forms/01-conditional-rendering/src/App.jsx
@@ -1,1 +1,14 @@
-
+import { useState } from 'react'; 
+function App() 
+{
+const [loggedIn, setLoggedIn] = useState(false); 
+return
+ (
+<div>
+<h2>Conditional Rendering</h2>
+{ loggedIn ? <p>Welcome User</p> : <p>Please Login</p> }
+<button onClick={()=>setLoggedIn(!loggedIn)}>{loggedIn ? "Logout" : "Login"} </button>
+</div>
+ );
+}
+export default App;

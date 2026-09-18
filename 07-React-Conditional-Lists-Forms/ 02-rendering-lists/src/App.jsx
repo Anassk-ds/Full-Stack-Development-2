cea3765fa183a2c43f5ca2f1a7038ca642977a1d
Anas Shaik <anasshaik382@gmail.com>
2026-09-18 21:32:46 +0530

    Update App.jsx

--- 07-React-Conditional-Lists-Forms/ 02-rendering-lists/src/App.jsx
+++ 07-React-Conditional-Lists-Forms/ 02-rendering-lists/src/App.jsx
@@ -1,1 +1,17 @@
-
+import React from "react";
+function App() 
+{
+const fruits = ["Apple", "Banana", "Cherry", "Date"];
+const fruitItems = [];
+for (let i = 0; i < fruits.length; i++) 
+ {
+fruitItems.push( <li> {fruits[i]} </li> );
+ }
+return (
+<div>
+<h2>Fruit List</h2>
+<ul>{fruitItems}</ul>
+</div>
+ );
+}
+export default App;
